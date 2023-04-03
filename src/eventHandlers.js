00a1fbeff96191b4f858c2529bc6039c23b61ad7
r253hmdryou <r253.hmdryou@gmail.com>
2023-04-04 03:58:08 +0900

    単一ファイルへ

--- src/eventHandlers.js
+++
@@ -1,4 +0,0 @@
-export async function handleAccountCreated(event) {
-  // Implement your business logic here
-  console.log(`Account created: ${event.id} with name ${event.name}`);
-}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {Spanner} = require('@google-cloud/spanner');
-const {handleAccountCreated} = require('./eventHandlers');
+
+function handleAccountCreated(event) {
+  // Implement your business logic here
+  console.log(`Account created: ${event.id} with name ${event.name}`);
+}
 
 const app = express();
 
