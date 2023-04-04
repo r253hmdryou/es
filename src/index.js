b4ccdac455e7607e07f442e8275893a15ca2cd55
r253hmdryou <r253.hmdryou@gmail.com>
2023-04-05 00:33:28 +0900

    マルチユーストランザクション？

--- src/index.js
+++ src/index.js
@@ -16,11 +16,7 @@ const database = instance.database('events');
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-  const query = {
-    sql: "SELECT 1",
-  };
-  const [rows] = await database.run(query);
-  res.send(`Hello from App Engine! ${JSON.stringify(rows[0])}`);
+  res.send(`Hello from App Engine!`);
 });
 
 app.post('/accounts', async (req, res) => {
@@ -35,21 +31,39 @@ app.post('/accounts', async (req, res) => {
   const aggregateId = "your-aggregate-id-generation-logic";
   const eventType = "AccountCreated";
   const eventData = JSON.stringify({ name });
-  const createdAt = new Date().valueOf();
+  const createdAt = new Date().toISOString();
 
-  await database.run({
-    sql: `
-      INSERT INTO event_store (id, aggregate_id, type, data, created_at)
-      VALUES (@id, @aggregateId, @type, @data, @createdAt)
-    `,
-    params: { id: eventId, aggregateId, type: eventType, data: eventData, createdAt },
-  });
+  try {
+    const [rowCount] = await database.runTransaction(async (err, transaction) => {
+      if (err || !transaction) {
+        console.error('Error while starting transaction:', err);
+        return;
+      }
 
-  // Call the event handler
-  // await handleAccountCreated(JSON.parse(eventData));
+      const rowCount = await transaction.runUpdate({
+        sql: `
+          INSERT INTO event_store (id, aggregate_id, type, data, created_at)
+          VALUES (@id, @aggregateId, @type, @data, @createdAt)
+        `,
+        params: { id: eventId, aggregateId, type: eventType, data: eventData, createdAt },
+      });
 
-  res.status(201).send({ id: aggregateId, name });
-})
+      await transaction.commit();
+      return rowCount;
+    });
+
+    if (rowCount > 0) {
+      // Call the event handler
+      await handleAccountCreated(JSON.parse(eventData));
+      res.status(201).send({ id: aggregateId, name });
+    } else {
+      res.status(500).send('Error occurred while saving the event');
+    }
+  } catch (err) {
+    console.error('Error while executing transaction:', err);
+    res.status(500).send('Error occurred while saving the event');
+  }
+});
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
